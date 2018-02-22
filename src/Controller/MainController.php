54a50658cd156d0a0cd01bf3dd6dcc788e51b446
Даир Байдаулетов <dair.baidauletov@gmail.com>
2018-02-22 23:21:26 +0200

    Phase 6 completed

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -241,84 +241,10 @@ class MainController extends Controller
      }
 
      /**
-      * @Route("/saveimages")
+      * @Route("/maps/database")
       */
-     public function saveImage()
+     public function database()
      {
-       if (isset($_GET['submit'])) {
-         if (!empty($_GET["address"])) {
-           $address = urlencode($_GET["address"]);
-           $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyBt3uKBhBC3dEBbvgOGkXcKzB8fQilcJDA";
-           $resp_json = file_get_contents($url);
-           $resp = json_decode($resp_json, true);
-         } else {
-           $lat = urlencode($_GET["lat"]);
-           $lng = urlencode($_GET["lng"]);
-           $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={$lat},{$lng}&key=AIzaSyBt3uKBhBC3dEBbvgOGkXcKzB8fQilcJDA";
-           $resp_json = file_get_contents($url);
-           $resp = json_decode($resp_json, true);
-         }
-         if ($resp['status']=='OK') {
-             $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
-             $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
-             $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
-             if ($lati && $longi && $formatted_address) {
-               return $this->render('maps/images.html.twig', array(
-                     'longitude' => $longi, 'latitude' => $lati, 'formatted_address'=> $formatted_address
-               ));
-             } else {
-               return false;
-             }
-           }
-           else {
-             echo "<strong>ERROR: {$resp['status']}</strong>";
-             return false;
-           }
-       } else {
-         return $this->render('maps/images.html.twig');
-       }
-
-       if($_SERVER["REQUEST_METHOD"] == "POST") {
-           // Check if file was uploaded without errors
-           if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-
-               $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-               $filename = $_FILES["photo"]["name"];
-               $filetype = $_FILES["photo"]["type"];
-               $filesize = $_FILES["photo"]["size"];
-
-               // Verify file extension
-               $ext = pathinfo($filename, PATHINFO_EXTENSION);
-               if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
-
-               // Verify file size - 5MB maximum
-               $maxsize = 5 * 1024 * 1024;
-               if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
-
-               // Verify MYME type of the file
-               if(in_array($filetype, $allowed)){
-                   // Check whether file exists before uploading it
-                   if(file_exists("upload/" . $_FILES["photo"]["name"])){
-                       echo $_FILES["photo"]["name"] . " is already exists.";
-                       return $this->redirect('maps/images', 308, array('message' => '$_FILES["photo"]["name"] . " is already exists."'));
-                   } else{
-                       // $destination_path = getcwd().DIRECTORY_SEPARATOR;
-                       // echo $destination_path;
-                       // $target_path = $destination_path . basename( $_FILES["photo"]["name"]);
-                       // move_uploaded_file($_FILES['photo']['tmp_name'], $target_path);
-                       move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $_FILES["photo"]["name"]);
-                       echo "Your file was uploaded successfully.";
-                       // return $this->redirect('maps/images', 308);
-
-                       return $this->redirect($this->generateUrl('maps/images', array('message' => 'Your file was uploaded successfully.')));
-                   }
-               } else {
-                   echo "Error: There was a problem uploading your file. Please try again.";
-                   return $this->redirect('maps/images', 308);
-               }
-           } else{
-               echo "Error: " . $_FILES["photo"]["error"];
-           }
-        }
-      }
-    }
+       return $this->render('maps/database.html.twig');
+     }
+  }
